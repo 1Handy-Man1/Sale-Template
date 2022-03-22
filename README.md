0da6153669f26ed199e9182d4ec8680eeb23d92f
TheHandMaster <69435603+Hshipp267@users.noreply.github.com>
2022-03-22 15:33:07 -0700

    Made a sale template

---
+++ README.md
@@ -0,0 +1,3 @@
+# Sales Template
+
+Making a template website purchasable for users
---
+++ app.js
@@ -0,0 +1,38 @@
+const express = require('express')
+const expressLayouts = require('express-ejs-layouts')
+const ejsLint = require('ejs-lint');
+
+const app = express()
+const PORT = process.env.PORT || 5000;
+
+// EJS Engine
+app.use(expressLayouts)
+app.set('view engine', 'ejs')
+app.use( express.static( "public" ) );
+// app.use(ejsLint)
+
+// Routes to the pages
+app.get('/', (req, res) => {
+    res.render('index', { title: "Home"})
+})
+app.get('/about', (req, res) => {
+    res.render('about', { title: "About Us"})
+})
+app.get('/contact', (req, res) => {
+    res.render('contact', { title: "Contact Us"})
+})
+app.get('/login', (req, res) => {
+    res.render('login', { title: "Login"})
+})
+app.get('/register', (req, res) => {
+    res.render('register', { title: "Register"})
+})
+
+// listing port 5000
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
+
+/* const start = async () => {
+    try{
+        
+    }
+} */
